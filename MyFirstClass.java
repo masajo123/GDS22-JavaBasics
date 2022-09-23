abe094d43ec5c475c32e8d22a7de751f8f028dfd
martina <09640404@edu.campus02.at>
2022-09-23 17:08:35 +0200

    My First Class created

---
+++ MyFirstClass.java
@@ -0,0 +1,13 @@
+public class MyFirstClass {
+    //unsere erste Klasse erzeugt
+
+    //psvm
+    public static void main(String[] args) {
+        //erste Ausgabe
+        System.out.println("Hello World");
+
+        //
+
+    }
+
+}
