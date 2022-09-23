5704e5d56e38b75a4292b20e3210930e61187e3e
martina <09640404@edu.campus02.at>
2022-09-23 18:29:22 +0200

    My First Class created1

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 1;
+        int secondNumber = 3;
+        int ergebnis = firstNumber + secondNumber;
+        String name = "Martina";
+
+        System.out.println("Meine Name ist: " + name);
+        System.out.println(firstNumber + "+" + secondNumber + "=" + ergebnis);
+
+    }
+}
